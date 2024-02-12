47cb94f874c8cf858ac09747d002a2f5d5f3ca09
dannarew <143512652+dannarew@users.noreply.github.com>
2024-02-12 00:02:15 -0500

    agony

--- Homework 3/Loops.java
+++ Homework 3/Loops.java
@@ -9,10 +9,34 @@ you need to explain why you picked your for loop conditional and what's doing th
 if you're confused reach out!
  */
 
+/* So once upon a time, I googled how to break a string down into individual characters.
+I learned of a couple methods that could be used, but the most simple thing wss checking the
+length of the strenth and comparing to the current index. So basically what we have here is...
+ */
+
+import java.util.Scanner; //import scanner
 public class Main {
     public static void main(String[] args) {
 
+        Scanner og = new Scanner(System.in); //make new scanner
+        System.out.print("Enter a string: "); //print out an input prompt
+        String word = og.nextLine(); //create a variable to store the input as a string
 
-
+        for(int i = 0; i < word.length(); i++){
+            System.out.println(word.charAt(i) + " ");{
+/*for loop that starts the with i being set 0 since we want to start at the first letter,
+we then check the length of the input word with .length which returns the amount of characters in
+a word starting at 0, and while i is less than the length of the word we print each index
+by incrementing i + 1, eventually giving us the whole word either spaced out on one line with print
+or one character per line as seen with println or sum like dat
+ */
+            }
+        }
     }
 }
+
+
+
+
+
+
--- Homework 3/LoopsDebug.java
+++ Homework 3/LoopsDebug.java
@@ -1,41 +1,74 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        Scanner og = new Scanner(System.in);
 
         //P1: This one only prints 0-9, can you fix it so it prints 1-10?
+        //we make it <= 10 so that it stops at 10 rather than 9
         System.out.println("Problem 1");
-        for (int i = 0; i < 10; i++){
+        for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
 
         //P2: Ask the user for a number. Create a loop to find the factorial of it
         //(factorial = X!, X being the user input, Factorials are every digit before X multiplied together)
         System.out.println("Problem 2");
-        System.out.println("Enter a number and I will tell you the fatorial: ");
-        //here's a hint
-        for (int i = 0; i < ?; i++){
+        System.out.println("Enter a number and I will tell you the factorial: ");
 
+        int input2 = og.nextInt();
+
+        long factorial = 1;
+
+        for (int i = 1; i <= input2; i++) {
+            factorial *= i;
         }
+        System.out.println("Factorial of " + input2 + " is: " + factorial);
 
-        //P3: Ask the user for a number, and then add together every OTHER digit (starting from 1)
+        //P3: Ask the user for a number, and then add together every OTHER digit (starting from index 0)
         System.out.println("Problem 3");
         System.out.println("Enter a number and I will tell you the sum of every other number: ");
-        //No hint! what do you need to complete this task?
 
+        int input3 = og.nextInt();
+
+        int sum = 0;
+        int index = 0;
+
+        while (input3 > 0) {
+            int digit = input3 % 10;
+
+            if (index % 2 == 0) {
+                sum += digit;
+            }
+
+            input3 /= 10;
+            index++;
+        }
+        System.out.println("Sum of every other digit: " + sum);
 
         //P4: Why does this loop never stop!
         //what can you do to break out of the loop after it prints once?
         System.out.println("Problem 4");
+
         boolean run = true;
-        while (run == true){
+
+        while (run == true) {
             System.out.println("I printed once!");
+            run = false;
         }
 
         //P5: Take a string from the user and print them the reverse!
         System.out.println("Problem 5");
-        //hint
-        String reverse = "";
 
+        System.out.println("Enter a string: ");
+
+        String input5 = og.next();
+
+        String reverse = "";
+        for (int i = input5.length() - 1; i >= 0; i--) {
+            reverse += input5.charAt(i);
+        }
+        System.out.println("Reversed string: " + reverse);
     }
 }
+
