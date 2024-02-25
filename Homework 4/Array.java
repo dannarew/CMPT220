ab61e7f7f06b60ae1664cc99cae7ad46b133e771
dannarew <143512652+dannarew@users.noreply.github.com>
2024-02-25 15:04:19 -0500

    wizz

--- Homework 4/Array.java
+++ Homework 4/Array.java
@@ -10,7 +10,36 @@ Take the given array and keep track of:
 public class Main {
     public static void main(String[] args) {
         //Don't let the size of this scare you! no matter how big an array is, it all works the same!
-        int[] myArray = {10,3,295,38,20,3,4,267,2445,10,5566,87,93,17,10,2,87,267,3176,3,82};
+        int[] myArray = {10, 3, 295, 38, 20, 3, 4, 267, 2445, 10, 5566, 87, 93, 17, 10, 2, 87, 267, 3176, 3, 82};
         //you cannot use the array util. Do this one by hand :(
+        int[] found = new int[myArray.length];
+
+        for (int i = 0; i < myArray.length; i++) {
+
+            for (int j = i + 1; j < myArray.length; j++) {
+
+                if (myArray[i] == myArray[j]) {
+
+                    if (found[i] == 0) {
+                        System.out.println("Number " + myArray[i] + " is a duplicate.");
+                    }
+                    found[i]++;
+                    found[j]++;
+                }
+            }
+        }
+
+        boolean duplicate = false;
+        for (int i = 0; i < myArray.length; i++) {
+            if (found[i] > 0) {
+
+                duplicate = true;
+                System.out.println(myArray[i] + " has" + " been seen " + (found[i] + 1) + " times ");
+                //seems to overprint/spam the occurences, but if it ain't broke don't fix it :)
+            }
+        }
+        if (!duplicate) {
+            System.out.println("No duplicates found");
+        }
     }
 }
