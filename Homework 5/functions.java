9193dae9a895ae1b601a0877b47b646cad159cf1
dannarew <143512652+dannarew@users.noreply.github.com>
2024-03-24 11:10:08 -0400

    straw

--- Homework 5/functions.java
+++ Homework 5/functions.java
@@ -5,9 +5,29 @@ please go a little more complex than just simple math/simple printing for the fu
 feel free to google some inspiration though :)
 */
 
-public class Main {
+public class functions {
     public static void main(String[] args) {
-    
-    }
+        int[] test = {17, 40, 27, 2, 393, 191};
+        upDown(test);
    
+}
+    public static void upDown(int[] arr) {
+        if (arr.length == 0) {
+            System.out.println("Try again! ");
+            return;
+        }
+        int min = arr[0];
+        int max = arr[0];
+    
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println("Lowest value is: " + min);
+        System.out.println("Highest value is: " + max);
+    }
 }
